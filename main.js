70e48d19825ca09d19951b231241c7fde1353ea6
anagaeidam <111796810+anagaeidam@users.noreply.github.com>
2022-10-20 16:54:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("Meu primeiro projeto no console");
+
+            var firstName = "Sophia";
+            var lastName = " Programadora";
+
+            var fullName = firstName.concat(lastName);
+            console.log(fullName);
+
+            var a = 10;
+            var b = 5;
+
+            var add = a + b ;
+            console.log(add);
+
+            var sub = a - b ;
+            console.log(sub);
+
+            var multiple = a * b ;
+            console.log(multiple);
+
+            var divide = a / b ;
+            console.log(divide);
